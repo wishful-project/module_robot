3652db7756547f7da4f4bdf9c191fd48738eb970
Piotr Gawlowicz <gawlowicz@tu-berlin.de>
2016-03-18 15:39:45 +0100

    simple impl

--- wishful_module_robot/robot_module.py
+++ wishful_module_robot/robot_module.py
@@ -13,28 +13,11 @@ __email__ = "johannes.kunkel@campus.tu-berlin.de"
 class RobotModule(wishful_module.AgentModule):
     def __init__(self):
         super(RobotModule, self).__init__()
-        self.log = logging.getLogger('wifi_module.main')
-        self.channel = 1
-        self.power = 1
+        self.log = logging.getLogger('RobotModule')
+        self.position = [12,323]
 
-    @wishful_module.bind_function(upis.radio.set_channel)
-    def set_channel(self, channel):
-        self.log.debug("Simple Module sets channel: {} on interface: {}".format(channel, self.interface))
-        self.channel = channel
-        return ["SET_CHANNEL_OK", channel, 0]
 
-    @wishful_module.bind_function(upis.radio.get_channel)
-    def get_channel(self):
-        self.log.debug("Simple Module gets channel of interface: {}".format(self.interface))
-        return self.channel
-
-    @wishful_module.bind_function(upis.radio.set_power)
-    def set_power(self, power):
-        self.log.debug("Simple Module sets power: {} on interface: {}".format(power, self.interface))
-        self.power = power
-        return {"SET_POWER_OK_value" : power}
-
-    @wishful_module.bind_function(upis.radio.get_power)
-    def get_power(self):
-        self.log.debug("Simple Module gets power on interface: {}".format(self.interface))
-        return self.power
+    @wishful_module.bind_function(upis.context.get_node_position)
+    def get_position(self):
+        self.log.debug("Get Node Position".format())
+        return self.position
